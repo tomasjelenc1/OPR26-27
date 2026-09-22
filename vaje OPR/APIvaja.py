5d822b85e8fb99c7df6f008e47be08157233275f
tomasjelenc1 <tomas.jelenc1@gmail.com>
2026-09-22 14:16:42 +0000

    drugi commit vaje

--- vaje OPR/APIvaja.py
+++ vaje OPR/APIvaja.py
@@ -6,11 +6,10 @@ imena = ["Svit", "Gaj", "Urban", "Žan"]
 
 
 for i, imena in enumerate(imena):
-    base_url = "https://api.agify.io?name={imena}"
+    base_url = f"https://api.agify.io?name={imena}"
     call = requests.get(base_url).json()
-    print(f"call["name"]")
-    print(f"call["age"]")
-
+    print(f"call['name']")
+    print(f"call['age']")
 
 
 
