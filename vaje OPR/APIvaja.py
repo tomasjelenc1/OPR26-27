2b16f9ffa6810294cfe7c6994954491d044d0fd9
tomasjelenc1 <tomas.jelenc1@gmail.com>
2026-09-22 07:38:49 +0000

    prvi komit vaje

---
+++ vaje OPR/APIvaja.py
@@ -0,0 +1,41 @@
+import requests
+
+#API osnovna vaja
+imena = ["Svit", "Gaj", "Urban", "Žan"]
+
+
+
+for i, imena in enumerate(imena):
+    base_url = "https://api.agify.io?name={imena}"
+    call = requests.get(base_url).json()
+    print(f"call["name"]")
+    print(f"call["age"]")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for each
+"""
+for i in imena:
+    print(i)
+
+# enumerate
+print(list(enumerate(imena)))
+
+for i in enumerate(imena):
+    print(i, imena)
+"""
